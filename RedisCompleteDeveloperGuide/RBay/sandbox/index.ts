10b14aba8ddc1e38266203418a8fffd75be902ab
Denver <70090559+Denver44@users.noreply.github.com>
2023-09-30 14:39:02 +0530

    issue with HGETALL

--- RedisCompleteDeveloperGuide/RBay/sandbox/index.ts
+++ RedisCompleteDeveloperGuide/RBay/sandbox/index.ts
@@ -17,5 +17,18 @@ const run = async () => {
 	await setObjectInHSet();
 	let cachedData = await getObjectFromHGetAll('car');
 	console.log('Cached data: ', cachedData);
+
+	cachedData = await getObjectFromHGetAll('car1');
+
+	if (!cachedData) {
+		// This will never work cachedData is always an object
+		console.log('Cached data: ', cachedData);
+		console.log('No data found in cache');
+	}
+
+	if (Object.keys(cachedData).length === 0) {
+		console.log('Cached data: ', cachedData);
+		console.log('No data found in cache');
+	}
 };
 run();
