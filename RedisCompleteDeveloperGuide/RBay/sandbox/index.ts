d48a6cfdaabf40537aac1c87141ec3d82441db1b
Denver <70090559+Denver44@users.noreply.github.com>
2023-09-30 14:42:35 +0530

    issue with HSET

--- RedisCompleteDeveloperGuide/RBay/sandbox/index.ts
+++ RedisCompleteDeveloperGuide/RBay/sandbox/index.ts
@@ -2,9 +2,16 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const setObjectInHSet = async () => {
+	// const carObj = {
+	// 	color: 'red',
+	// 	year: 1950
+	// };
 	const carObj = {
 		color: 'red',
-		year: 1950
+		year: 1950,
+		engine: { cylinder: 2 }, // nested object will not be array it will get stringified and stored as string and will be returned as string engine: '[object Object]',
+		owner: null || '', // we cannot do JSON.stringify on null so this will throw error so to save this we have to use ''
+		number: undefined || '' // we cannot do JSON.stringify on undefined so this will throw error so to save this we have to use ''
 	};
 	return client.hSet('car', carObj);
 };
@@ -17,18 +24,5 @@ const run = async () => {
 	await setObjectInHSet();
 	let cachedData = await getObjectFromHGetAll('car');
 	console.log('Cached data: ', cachedData);
-
-	cachedData = await getObjectFromHGetAll('car1');
-
-	if (!cachedData) {
-		// This will never work cachedData is always an object
-		console.log('Cached data: ', cachedData);
-		console.log('No data found in cache');
-	}
-
-	if (Object.keys(cachedData).length === 0) {
-		console.log('Cached data: ', cachedData);
-		console.log('No data found in cache');
-	}
 };
 run();
