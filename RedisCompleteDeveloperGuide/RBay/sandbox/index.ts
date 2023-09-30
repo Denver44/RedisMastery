a87d654acfefb8945a86d5c3a4466e3d2b01b13d
Denver <70090559+Denver44@users.noreply.github.com>
2023-09-30 18:59:03 +0530

    HSET and HGETALL Issues

--- RedisCompleteDeveloperGuide/RBay/sandbox/index.ts
+++ RedisCompleteDeveloperGuide/RBay/sandbox/index.ts
@@ -2,9 +2,16 @@ import 'dotenv/config';
 import { client } from '../src/services/redis';
 
 const setObjectInHSet = async () => {
+	// const carObj = {
+	// 	color: 'red',
+	// 	year: 1950
+	// };
 	const carObj = {
 		color: 'red',
-		year: 1950
+		year: 1950,
+		engine: { cylinder: 2 }, // nested object will not be array it will get stringified and stored as string and will be returned as string engine: '[object Object]',
+		owner: null || '', // we cannot do JSON.stringify on null so this will throw error so to save this we have to use ''
+		number: undefined || '' // we cannot do JSON.stringify on undefined so this will throw error so to save this we have to use ''
 	};
 	return client.hSet('car', carObj);
 };
