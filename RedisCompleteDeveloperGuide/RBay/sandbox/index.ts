c060ce8541b37d86eb8d869a1047939533c2df9e
Denver <70090559+Denver44@users.noreply.github.com>
2023-09-30 14:15:39 +0530

    HSET and HGETALL

--- RedisCompleteDeveloperGuide/RBay/sandbox/index.ts
+++ RedisCompleteDeveloperGuide/RBay/sandbox/index.ts
@@ -1,5 +1,21 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const setObjectInHSet = async () => {
+	const carObj = {
+		color: 'red',
+		year: 1950
+	};
+	return client.hSet('car', carObj);
+};
+
+const getObjectFromHGetAll = async (key) => {
+	return client.hGetAll(key);
+};
+
+const run = async () => {
+	await setObjectInHSet();
+	let cachedData = await getObjectFromHGetAll('car');
+	console.log('Cached data: ', cachedData);
+};
 run();
